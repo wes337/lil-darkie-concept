358d318e261b4cac4e0e91832fcf99986aeab9c8
wes337 <wesmoses@gmail.com>
2023-06-15 09:09:05 +0300

    some scrolling effects

--- src/routes/fall-tour.jsx
+++ src/routes/fall-tour.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 // import { Link } from "react-router-dom";
 import useStore from "../store";
+import { isMobileSizedScreen } from "../utils";
 import title from "../images/tour/fall-tour-title.png";
 import end from "../images/tour/end.png";
 import bottom from "../images/tour/bottom.png";
 import voteGuy from "../images/vote_guy.png";
 import tourDates from "../data/fall-tour-dates.json";
+import paratrooper from "../images/paratrooper.png";
 import "../styles/fall-tour.scss";
 
 export default function FallTour() {
@@ -19,12 +21,33 @@ export default function FallTour() {
 
   return (
     <div className="fall-tour">
-      <img className="title" src={title} alt="" />
-      <img className="end" src={end} alt="" />
+      <img
+        className="title"
+        src={title}
+        alt=""
+        style={{
+          transform: `translateY(${
+            isMobileSizedScreen() ? (scroll - 2) * -1 : scroll + 5 * 2
+          }px)`,
+        }}
+      />
+      <img
+        className="end"
+        src={end}
+        alt=""
+        style={{
+          marginTop: isMobileSizedScreen()
+            ? Math.floor(scroll * -1) * 1.2
+            : Math.floor(scroll * 1.75),
+        }}
+      />
       <img className="bottom" src={bottom} alt="" />
       <div className={`panel${scroll >= 100 ? " scroll" : ""}`}>
         <div className="vote-guy">
           <img src={voteGuy} alt="" />
+        </div>
+        <div className="paratrooper">
+          <img src={paratrooper} alt="" />
         </div>
         <div className="tour-cities">
           {tourDates.map(({ date, city, venue, ticketLink }) => {
